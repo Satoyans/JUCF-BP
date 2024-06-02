4c087db2fc55baf284c9d596705c154be314272f
satoyan <satoyan.discord@gmail.com>
2024-06-02 21:57:50 +0900

    構造変更、ホバーテキスト追加

--- scripts/encode.js
+++ scripts/encode.js
@@ -45,22 +45,21 @@ function data2sendText_inside(...data) {
 
 /**
  *
- * @param {{x:number, y:number, w:number, h:number, is_show_button:boolean, is_show_image:boolean, is_show_text:boolean, text:string, image:string }[]} ui_elements
+ * @param {{x:number, y:number, w:number, h:number, is_show_button:boolean, is_show_image:boolean, is_show_text:boolean, text:string, image:string, button_hover_text:string }[]} ui_elements
  * @param {{x:string, y:string}} form_size
  * @returns
  */
 const RP_screen_size = { x: 465, y: 262 };
 export function encode(ui_elements, form_size) {
 	//JSONUIの最大[465, 262]
-
 	const offset_x_inc = (RP_screen_size.x - form_size.x) / 2;
 	const offset_y_inc = (RP_screen_size.y - form_size.y) / 2;
 	const output_obj = [];
 	let count = 0;
 	for (let ui_element of ui_elements) {
 		count += 1;
-		const { x, y, w, h, is_show_button, is_show_image, is_show_text, text, texture } = ui_element;
-		//ui_elementにis_show_closeを追加するか迷う
+		const { x, y, w, h, is_show_button, is_show_image, is_show_text, text, texture, button_hover_text } = ui_element;
+
 		let data1 = "";
 		if (is_show_text) data1 += "text";
 		if (is_show_image) data1 += "image";
@@ -68,10 +67,10 @@ export function encode(ui_elements, form_size) {
 		//ui_elementにis_show_closeを追加するか迷う
 		if (ui_element.is_show_close === true) data1 += "close";
 		let data2 = text;
-		console.warn(JSON.stringify({ x: offset_x_inc + x - count, y: offset_y_inc + y - 1 }));
-		let data3_temp = [`${w}`, `${h}`, `${offset_x_inc + x - count}`, `${offset_y_inc + y - 1}`];
-		let data3 = data2sendText_inside(...data3_temp);
-		let send_text = data2sendText(data1, data2, data3);
+		let data3 = button_hover_text;
+		let data4_temp = [`${w}`, `${h}`, `${offset_x_inc + x - count}`, `${offset_y_inc + y - 1}`];
+		let data4 = data2sendText_inside(...data4_temp);
+		let send_text = data2sendText(data1, data2, data3, data4);
 		output_obj.push({ text: send_text, image: texture });
 	}
 	return output_obj;
--- scripts/main.js
+++ scripts/main.js
@@ -3,15 +3,85 @@ import { ActionFormData } from "@minecraft/server-ui";
 import FORM_DATA from "./form_data";
 import { encode } from "./encode";
 
-system.afterEvents.scriptEventReceive.subscribe((ev) => {
+const aux_x = 14;
+const aux_y = 29;
+const aux_offset_x = -7;
+const aux_offset_y = -22;
+
+function createElements(form_data) {
+	const { x, y, title, elements } = form_data;
+	const form_flame_elements = [
+		{
+			//背景
+			is_show_text: false,
+			is_show_image: true,
+			is_show_button: false,
+			button_hover_text: "",
+			w: x + aux_x - 8,
+			h: y + aux_y - 8,
+			x: aux_offset_x + 4,
+			y: aux_offset_y + 4,
+			text: "",
+			texture: "textures/ui/translucent_black",
+		},
+		{
+			//枠
+			is_show_text: false,
+			is_show_image: true,
+			is_show_button: false,
+			button_hover_text: "",
+			w: x + aux_x,
+			h: y + aux_y,
+			x: aux_offset_x,
+			y: aux_offset_y,
+			text: "",
+			texture: "textures/ui/dialog_background_hollow_3",
+		},
+		{
+			//X
+			is_show_text: false,
+			is_show_image: false,
+			is_show_button: false,
+			is_show_close: true,
+			button_hover_text: "",
+			w: x + aux_x,
+			h: y + aux_y,
+			x: aux_offset_x,
+			y: aux_offset_y,
+			text: "",
+			texture: "",
+		},
+		{
+			//Title
+			is_show_text: true,
+			is_show_image: false,
+			is_show_button: false,
+			button_hover_text: "",
+			w: x + aux_x,
+			h: 30,
+			x: aux_offset_x,
+			y: aux_offset_y,
+			// text: `§0${title}${x}x${y}(${x + aux_x}x${y + aux_y})`,
+			text: `§0${title}`,
+			texture: "",
+		},
+	];
+	return encode(form_flame_elements.concat(...elements), { x, y });
+}
+
+//動的インポートができないからモジュール評価のタイミングで計算しちゃう
+const FORM_ELEMENTS_DATA = createElements(FORM_DATA);
+
+system.afterEvents.scriptEventReceive.subscribe(async (ev) => {
 	if (ev.id !== "cf:form") return;
 	const sender = ev.sourceEntity;
 	if (!sender) return;
 	if (sender.typeId !== "minecraft:player") return;
-	const form = new ActionFormData().title(ev.message !== "" ? ev.message : "§c§u§s§t§o§m§f§o§r§m");
-	const ui_elements = FORM_DATA[0];
-	const form_size = FORM_DATA[1];
-	for (let data of encode(ui_elements, form_size)) {
+
+	const raw_title_string = ev.message !== "" ? ev.message : "§c§u§s§t§o§m§f§o§r§m";
+	const form = new ActionFormData().title(raw_title_string);
+
+	for (let data of FORM_ELEMENTS_DATA) {
 		const { text, image } = data;
 		form.button(text, image);
 	}
