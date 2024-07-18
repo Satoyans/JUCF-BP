b0db16f67d3227291fa649362997bbf600a307a8
satoyan <satoyan.discord@gmail.com>
2024-07-18 16:24:49 +0900

    型を外に分けた

--- ts/forms.ts
+++ ts/forms.ts
@@ -1,16 +1,17 @@
 import { Player } from "@minecraft/server";
 import { customForm, resultType } from "./class";
 
-export default <
-	{
-		[key: string]: {
-			form: (player: Player) => customForm;
-			response: (result: resultType, player: Player) => void;
-		};
-	}
->{
-	scriptAPI_form: {
-		form: (player) => new customForm({ x: 100, y: 100 }, "scriptAPI_form", true).addElement("button", 100, 100, 0, 0, "ボタン"),
+type formsType = {
+	[key: string]: {
+		form: (player: Player) => customForm;
+		response: (result: resultType, player: Player) => void;
+	};
+};
+
+//フォームを作成して返す関数と、フォームのレスポンスを受け取って処理する関数を書く
+export default <formsType>{
+	example_form: {
+		form: (player) => new customForm({ x: 100, y: 100 }, "example_form", true).addElement("button", 100, 100, 0, 0, "ボタン"),
 		response: (result, player) => {
 			if (result.selectedLabel === "ボタン") player.sendMessage("ボタンを押した！");
 		},
