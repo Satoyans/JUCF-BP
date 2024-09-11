2fc6d2f6af00ed25fce8f94ad11ec8dc00e25f0b
satoyan <satoyan.discord@gmail.com>
2024-09-11 16:57:24 +0900

    分かりやすい動きのある見た目に変更

--- ts/forms.ts
+++ ts/forms.ts
@@ -70,33 +70,65 @@ export default <formsType>{
 		},
 	},
 	//@minecraft/server-ui@1.3.0-betaのUiManagerが必要
-	//更新フォーム: {
-	//	form: (player: Player, message: string) => {
-	//		let count = Number(message);
-	//		if (Number.isNaN(count)) count = 0;
-	//		const form = new customForm({ x: 200, y: 150 }, "チャット", true);
-	//		form.addElement("text", 10, 10, 0, 10, `${new Date().toDateString()}`);
-	//		form.addElement("text", 10, 10, 0, 20, `count:${count + 1}`);
-	//
-	//		const player_name = player.name;
-	//		chat_form_set.add(player_name);
-	//		system.runTimeout(() => {
-	//			if (!chat_form_set.has(player_name)) return;
-	//			chat_form_delete_flag_set.add(player_name);
-	//			uiManager.closeAllForms(player);
-	//			const send_players = world.getPlayers({ name: player_name });
-	//			if (send_players.length === 0) return;
-	//			const send_player = send_players[0];
-	//			send_player.runCommand(`scriptevent cfs:更新フォーム ${count + 1}`);
-	//		}, 20);
-	//
-	//		return form;
-	//	},
-	//	response: (result, player) => {
-	//		if (!chat_form_delete_flag_set.has(player.name)) chat_form_set.delete(player.name);
-	//		chat_form_delete_flag_set.delete(player.name);
-	//	},
-	//},
+	/*
+	更新フォーム: {
+		form: (player: Player, message: string) => {
+			let count = Number(message);
+			if (Number.isNaN(count)) count = 0;
+			const form = new customForm({ x: 200, y: 150 }, "チャット", false);
+			form.addElement("image", 200, 150, 0, 0, "textures/blocks/iron_block");
+
+			const x = 200;
+			const y = 150;
+			const box_size = 20;
+			//for (let i in Array.from(Array(count))) {
+			//	const index = Number(i);
+			//	if ((count - index) * 10 > y) continue;
+			//	form.addElement("image", box_size, box_size, (index % Math.ceil(x / box_size)) * box_size, (count - index - 1) * box_size, "textures/blocks/gold_block");
+			//}
+
+			for (let x_index in Array.from(Array(Math.ceil(x / box_size)))) {
+				for (let y_index in Array.from(Array(Math.ceil(y / box_size)))) {
+					if ((Number(x_index) + Number(y_index) + count) % 2 === 0) continue;
+					form.addElement("image", box_size, box_size, Number(x_index) * box_size, Number(y_index) * box_size, "textures/blocks/gold_block");
+				}
+			}
+
+			const player_name = player.name;
+			//フォームを更新する対象
+			chat_form_set.add(player_name);
+			player.inputPermissions.cameraEnabled = false;
+			player.inputPermissions.movementEnabled = false;
+			system.runTimeout(() => {
+				//そのプレイヤーが対象にまだ入っているか
+				if (!chat_form_set.has(player_name)) return;
+				//更新時に閉じる必要があり、それがscript側のものであると示すためのset
+				chat_form_delete_flag_set.add(player_name);
+
+				uiManager.closeAllForms(player);
+				player.runCommand(`scriptevent cfs:更新フォーム ${count + 1}`);
+			}, interval);
+			return form;
+		},
+		response: (result, player) => {
+			if (chat_form_delete_flag_set.has(player.name)) {
+			} else {
+				//プレイヤーがフォームを意図的に閉じた時
+				chat_form_set.delete(player.name);
+				uiManager.closeAllForms(player);
+				player.inputPermissions.cameraEnabled = true;
+				player.inputPermissions.movementEnabled = true;
+
+				system.runTimeout(() => uiManager.closeAllForms(player), interval + 1);
+				system.runTimeout(() => chat_form_set.delete(player.name), 1);
+			}
+			//フラグリセット
+			chat_form_delete_flag_set.delete(player.name);
+		},
+	},
+	*/
 };
-const chat_form_set = new Set<string>();
-const chat_form_delete_flag_set = new Set<string>();
+//更新フォーム用
+//const interval = 20 * 0.5;
+//const chat_form_set = new Set<string>();
+//const chat_form_delete_flag_set = new Set<string>();
